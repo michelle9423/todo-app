b2a04b76c219c3f73dd0d2c64b342691f8123625
michelle9423 <michelle9423@gmail.com>
2026-04-22 14:09:13 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDYNqiGgcRLkneV7sJvCjNaLRyYoU12Uw8",
+  authDomain: "todo-app-fad5d.firebaseapp.com",
+  databaseURL: "https://todo-app-fad5d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "todo-app-fad5d",
+  storageBucket: "todo-app-fad5d.firebasestorage.app",
+  messagingSenderId: "186655580356",
+  appId: "1:186655580356:web:e6bc08c90fc7253184a991"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
